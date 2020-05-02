7bcb365afbf79021da16bd1b1814b076074ad9b6
nvshah <nipunshah6776@gmail.com>
2020-05-02 19:39:19 +0530

    Adding ListTile Widget for each transaction Replace Row/Column implementation wit ListTile in Card

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -26,8 +26,8 @@ class TransactionList extends StatelessWidget {
                   height: 200,
                   child: Image.asset(
                     'assets/images/waiting.png',
-                    fit: BoxFit
-                        .cover, // this makes image to fit to it's surrounding
+                    // this makes image to fit to it's surrounding
+                    fit: BoxFit.cover,
                   ),
                 ), //Image need container as a parent inorder to fit appropriate otherwise it will not fit to column as column takes as much height as it got
               ],
@@ -35,51 +35,36 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (ctxt, index) {
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            // give reference/tunnel to theme data; define in MateriaApp
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          // Display the amount in always 2 decimal places
-                          '\$${transactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).primaryColor,
+                  elevation: 6,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6.0),
+                        child: FittedBox(
+                          // Amount | Display the amount in always 2 decimal places
+                          child: Text(
+                            '\$${transactions[index].amount.toStringAsFixed(2)}',
                           ),
                         ),
                       ),
-                      Column(
-                        //Aligning All details on left side
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          // Title
-                          Text(
-                            transactions[index].title,
-                            // fetching style from global style define in MaterialApp
-                            style: Theme.of(context).textTheme.title,
-                          ),
-                          // Date
-                          Text(
-                            DateFormat.yMMMd().format(transactions[index].date),
-                            style: TextStyle(
-                              color: Colors.blueGrey,
-                            ),
-                          ),
-                        ],
+                    ),
+                    //Title
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    //Date
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                      style: TextStyle(
+                        color: Colors.blueGrey,
                       ),
-                    ],
+                    ),
                   ),
                 );
               },
